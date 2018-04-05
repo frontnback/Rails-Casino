301adafa149ffe319bd6c26f7069506febd0ae86
Eddie Alex <edmlounge@gmail.com>
2018-04-04 23:31:49 -0400

    Added autocomplete scripts for AJAX user search

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,4 +5,10 @@ $( document ).on('turbolinks:load', function() {
   $('body').fadeIn(2000);
   $('#flash-msg').fadeIn(3000);
   $('#flash-msg').fadeOut(8000);
+  $('#search-field').on('input', function() {
+    Rails.fire(this.form, 'submit');
+  });
+  // $('#search-form').on('submit', function(e) {
+  //   e.preventDefault();
+  // });
 });
